ffe511a2539cd7139b0612488f19d0e78ae07192
Ribas13 <diogo10ribeiro.dr@gmail.com>
2023-05-04 19:38:29 +0100

    updates

--- Makefile
+++ Makefile
@@ -3,10 +3,34 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: diosanto <diosanto@student.42.fr>          +#+  +:+       +#+         #
+#    By: diosanto <diosanto@student.42lisboa.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/02/24 15:39:10 by diosanto          #+#    #+#              #
-#    Updated: 2023/02/24 17:16:17 by diosanto         ###   ########.fr        #
+#    Updated: 2023/05/04 19:35:14 by diosanto         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+NAME	= pipex
+
+CC		= cc
+CFLAGS	= -Wall -Wextra -Werror #-g -fsanitize=address
+RM		= rm -f
+
+SRCS 	=	pipex.c
+
+OBJS	= $(SRCS:c=o)
+
+all: $(NAME) 
+
+$(NAME): $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) -o $(NAME)
+
+clean:
+	$(RM)  $(OBJS)
+
+fclean: clean
+	$(RM) $(NAME)
+
+re:  fclean $(NAME)
+
+.PHONY: all clean fclean re
--- pipex.c
+++ pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: diosanto <diosanto@student.42.fr>          +#+  +:+       +#+        */
+/*   By: diosanto <diosanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 15:38:49 by diosanto          #+#    #+#             */
-/*   Updated: 2023/02/24 17:16:14 by diosanto         ###   ########.fr       */
+/*   Updated: 2023/05/04 19:36:28 by diosanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int	main(int ac, char **av)
+int	main()
 {
-	int	id;
-	int	fd[2];
-	int	x;
-	int	y;
-
-	if (pipe(fd) == -1)
-	{
-		printf("ERROR");
-		return (1);
-	}
-	id = fork();
-	if (id == 0)
-	{
-		close(fd[0]);
-		printf("Input a number: \n");
-		scanf("%d", &x);
-		write(fd[1], &x, sizeof(int));
-		close(fd[1]);
-	}
-	else
-	{
-		close(fd[1]);
-		read(fd[0], &y, sizeof(int));
-		close(fd[0]);
-		printf("Got from child process: %d\n", y);
-	}
+	fork();
+	printf("Hello world\n");
 	return (0);
 }
