9cf75b61df9aab812d2f03ecb780f0a6e05e5b2f
Ribas13 <diogo10ribeiro.dr@gmail.com>
2023-05-04 20:03:47 +0100

    updates

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: diosanto <diosanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 15:38:49 by diosanto          #+#    #+#             */
-/*   Updated: 2023/05/04 19:36:28 by diosanto         ###   ########.fr       */
+/*   Updated: 2023/05/04 20:03:38 by diosanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
-int	main()
+int	main(void)
 {
 	fork();
-	printf("Hello world\n");
+	fork();
+	printf("Hello\n");
 	return (0);
 }
+
+
+//https://reactive.so/42-a-comprehensive-guide-to-pipex
+//https://csnotes.medium.com/pipex-tutorial-42-project-4469f5dd5901
+//https://www.youtube.com/playlist?list=PLfqABt5AS4FkW5mOn2Tn9ZZLLDwA3kZUY
+//https://www.youtube.com/watch?v=tcYo6hipaSA&list=PLfqABt5AS4FkW5mOn2Tn9ZZLLDwA3kZUY&index=2
